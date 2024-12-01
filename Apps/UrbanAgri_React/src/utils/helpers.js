75a3032c1a02064811f6b4aed88a231475bbd4ab
Kismet Krystle <kismetaievolution@gmail.com>
2024-12-02 03:47:01 +0800

    Reorganizing files for clarity

--- Apps/UrbanAgri_React/src/utils/helpers.js
+++
@@ -1,8 +0,0 @@
-import { ethers } from "ethers";
-
-// Helper function to parse Ether values to Wei
-const parseEther = (value) => {
-  return ethers.utils.parseEther(value.toString());
-};
-
-export { parseEther };
